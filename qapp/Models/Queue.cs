c77da11782481e576b714dd51f16748a8606b472
Gil Sadis <gil@licensario.com>
2012-03-15 11:51:14 +0200

    splitted classes to files

---
+++ qapp/Models/Queue.cs
@@ -0,0 +1,7 @@
+﻿namespace qapp.Models {
+    public class Queue
+    {
+        public string MerchantId { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ qapp/Models/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace qapp.Models
+{
+    public class Ticket
+    {
+        public string Id { get; set; }
+        public string QueueId { get; set; }
+        public string ProviderId { get; set; }
+        public string UserId { get; set; }
+
+
+        public static Ticket GetTicket(string providerId, string queueId, string userId) {
+            using (var session = MvcApplication.Store.OpenSession()) {
+                return
+                    session.Query<Ticket>().Where(
+                        t => t.ProviderId == providerId && t.QueueId == queueId && t.UserId == userId).FirstOrDefault();
+            }
+        }
+    }
+}
