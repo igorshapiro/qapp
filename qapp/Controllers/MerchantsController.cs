4cec10ce4b2a1c7c986263dcd59072f23369660a
Igor Shapiro <igor@licensario.com>
2012-03-15 10:01:13 +0200

    Added Test & Merchants controllers

---
+++ qapp/Controllers/MerchantsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace qapp.Controllers
+{
+    public class MerchantsController : Controller
+    {
+        //
+        // GET: /Merchants/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
