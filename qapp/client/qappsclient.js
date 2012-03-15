3ef321e4884d6fc59b60649b7a244f75f81f4cb6
Yoni Gross <gross.yoni@gmail.com>
2012-03-15 20:20:52 +0200

    server communication test

--- qapp/client/qappsclient.js
+++ qapp/client/qappsclient.js
@@ -1,5 +1,45 @@
-function getCategories(){  
+
+function addCategory(category){
+	var newOption = new Option(category);
+	newOption.value = category;
+	newOption.innerHTML = category;
+	$("#selectCategory").append(newOption);
+}
+
+function addPlace(place){
+	$("#placesList").append("<li><a>" + place.name + "</a></li>");
+	$("#placesList").listview("refresh");
+	
+}
+
+function initCategories(categories){
+	if (categories==null){
+		categories = ["Banks","Shops"];
+	}
+	for (var i in categories){
+		addCategory(categories[i]);
+	}
+}
+
+function initPlaces(places){
+	if (places==null){
+		var jsonStr = '[{name: "place1"}, {name: "place2"}, {name: "place3"}]';
+		places = eval('(' + jsonStr + ')');
+	}
+	
+	for (var i in places){
+		addPlace(places[i]);
+	}
+}
+
+function setCategories(){  
 	$.get("http://qapp.apphb.com/categories", function(data){
-        	alert("Data Loaded: " + data);
+        	initCategories(data);
         	});
+}
+
+function setPlaces(){
+	$.get("http://qapp.apphb.com/merchants", function(data){
+    	initCategories(data);
+    	});
 }
