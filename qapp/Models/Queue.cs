1cc30f14902f581d64f5cd014f062e89b6227e88
Igor Shapiro <igor@licensario.com>
2012-03-15 15:40:44 +0200

    Implemented: 	POST queueTickets/Next 	DELETE queueTickets 	POST queueTickets

--- qapp/Models/Queue.cs
+++ qapp/Models/Queue.cs
@@ -3,5 +3,23 @@
     {
         public string MerchantId { get; set; }
         public string Id { get; set; }
+        /// <summary>
+        /// Format:
+        ///     YYYYMMDDXXX
+        /// </summary>
+        public long CurrentPosition { get; set; }
+        public long LastPosition { get; set; }
+
+/*        public Ticket CreateTicket(string userId)
+        {
+            CurrentPosition++;
+            var ticket = new Ticket
+            {
+                UserId = userId,
+                ProviderId = MerchantId,
+                QueueId = Id
+            };
+            return ticket;
+        }*/
     }
 }
